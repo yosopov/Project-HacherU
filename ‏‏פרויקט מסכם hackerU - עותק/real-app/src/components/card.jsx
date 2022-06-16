10e1d87b2c396f253a52a3f9093e920b22c77d76
yosopov <58524131+yosopov@users.noreply.github.com>
2022-06-16 18:35:18 +0300

    Update card.jsx

--- ‏‏פרויקט מסכם hackerU - עותק/real-app/src/components/card.jsx
+++ ‏‏פרויקט מסכם hackerU - עותק/real-app/src/components/card.jsx
@@ -31,7 +31,7 @@ const Card = ({ card,addCard,deleteProduct,order}) => {
   return (
     <div  className="col-sm-12 col-md-6 col-xl-3 mt-3">
       <div style={{height:"100%"}} className="card border border-dark">
-        <img
+        <img style={{height:"300px"}}
           className="p-2 m-auto img-thumbnail"
           src={card.productImage}
           alt={card.productName}
